132e15bec2d29dd7d53bffcfe5f0ad4341711a01
gugibv <gugibv@163.com>
2023-03-02 22:18:53 +0800

    rm 06

--- 06-customertype-jpa/src/main/java/com/grey/repository/MoneyBeanRepository.java
+++
@@ -1,7 +0,0 @@
-package com.grey.repository;
-
-import com.grey.entity.converter.MoneyBean;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MoneyBeanRepository extends JpaRepository<MoneyBean,String> {
-}
--- 06-customertype-jpa/src/main/java/com/grey/repository/StudentBeanRepository.java
+++
@@ -1,7 +0,0 @@
-package com.grey.repository;
-
-import com.grey.entity.type.StudentBean;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentBeanRepository extends JpaRepository<StudentBean,String> {
-}
